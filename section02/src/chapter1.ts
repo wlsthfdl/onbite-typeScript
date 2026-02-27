9511db0971e7c240726fc27b54ec3b091a304dd8
Your Name <you@example.com>
2026-02-27 09:10:27 +0900

    Chap2. 타입스크립트 기본 13

---
+++ section02/src/chapter1.ts
@@ -0,0 +1,24 @@
+//number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = Infinity;
+let num4: number = -Infinity;
+let num5: number = NaN;
+
+//string
+let str1: string = "hello";
+let str2: string = `hello${num1}`;
+
+//boolean
+let boolean1: boolean = true;
+let boolean2: boolean = false;
+
+//undifined
+let unde1: undefined = undefined;
+
+//null: null타입엔 null을 써도 된다.
+let null1: null = null;
+
+//리터럴 타입
+let numA: 10 = 10;
+//numA = 12;                10만 가능
---
+++ section02/src/chapter2.ts
@@ -0,0 +1,20 @@
+//배열
+let numArr: number[] = [1, 2, 3];
+
+let strArr: string[] = ["hello", "im", "jinsol"];
+
+let boolArr: Array<boolean> = [true, false, false]; //제네릭
+
+//배열에 들어가는 요소들 타입이 다양할 경우
+let multiArr: (number | string)[] = [1, "hello1"];
+
+//다차원 배열의 타입을 정의하는 방법
+let doubleArr: number[][] = [
+  [1, 2, 3],
+  [4, 5],
+];
+
+//튜플: 길이와 타입이 고정된 배열
+let tup1: [number, number] = [1, 2];
+
+let tup2: [number, string, boolean] = [1, "2", true];
