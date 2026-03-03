954d8dd3ad30036b70f010d6853f66b512b74b79
Your Name <you@example.com>
2026-03-03 10:11:52 +0900

    27. 서로소 유니온 타입

---
+++ section03/src/chapter8.ts
@@ -0,0 +1,43 @@
+/**
+ * 서로소 유니온 타입
+ * 교집합이 없는 타입들로만 만든
+ */
+
+type Admin = {
+  tag: "ADMIN";
+  name: string;
+  kickCount: number;
+};
+
+type Member = {
+  tag: "MEMBER";
+  name: string;
+  point: number;
+};
+
+type Guest = {
+  tag: "GUEST";
+  name: string;
+  visitCount: number;
+};
+
+type User = Admin | Member | Guest;
+
+function login(user: User) {
+  switch (user.tag) {
+    case "ADMIN": {
+      console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
+      break;
+    }
+    case "MEMBER": {
+      console.log(`${user.name}님 현재까지 ${user.point}포인트 모았습니다.`);
+      break;
+    }
+    case "GUEST": {
+      console.log(
+        `${user.name}님 현재까지 ${user.visitCount}번 방문하셨습니다.`,
+      );
+      break;
+    }
+  }
+}
